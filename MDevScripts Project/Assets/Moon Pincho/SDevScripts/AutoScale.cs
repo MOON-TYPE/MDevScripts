1b2eb974ea64eee6880b5507b8e8e7bb366a6858
MoonAntonio <pinchodev91@gmail.com>
2017-03-02 15:18:18 +0100

    Preparando AutoScale

---
+++ MDevScripts Project/Assets/Moon Pincho/SDevScripts/AutoScale.cs
@@ -0,0 +1,20 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// AutoScale.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (Moon Pincho) 									        \\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonPincho
+{
+	public class AutoScale : MonoBehaviour 
+	{
+
+	}
+}
