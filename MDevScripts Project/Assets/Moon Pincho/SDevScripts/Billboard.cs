45a8e7876d4518d67dde2b152394dc8ca1b2b4ee
MoonAntonio <pinchodev91@gmail.com>
2017-03-02 15:52:47 +0100

    Preparando BillBoard

---
+++ MDevScripts Project/Assets/Moon Pincho/SDevScripts/Billboard.cs
@@ -0,0 +1,24 @@
+//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Billboard.cs (02/03/2017)													\\
+// Autor: Antonio Mateo (Moon Pincho) 									        \\
+// Descripcion:		Rota el objeto para que siempre este mirando a camara		\\
+// Fecha Mod:		02/03/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonPincho.MDevScripts
+{
+	/// <summary>
+	/// <para>Rota el objeto para que siempre este mirando a camara</para>
+	/// </summary>
+	[AddComponentMenu("MoonPincho/Dev/Billboard")]
+	public class Billboard : MonoBehaviour 
+	{
+
+	}
+}
